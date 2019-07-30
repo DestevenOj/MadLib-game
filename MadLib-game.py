c2732b06e3641803d48d63f24ebd2e5930ac54cf
Stephen Ochieng <stephenojwang1040@gmail.com>
2019-07-31 01:47:17 +0300

    Python MadLib Game

---
+++ MadLib-game.py
@@ -0,0 +1,7 @@
+color = input("Input your favourite color:")
+animal = input("Input an animal you fear most:")
+city = input("Input your favourite city in Africa:")
+
+print (color + " is beauty !")
+print (animal + " are fierce ")
+print (city + " is a modern City")
